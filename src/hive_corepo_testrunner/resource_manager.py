1a5a52b16e9573b912f899e18991fc14838e7ff6
Mads Bondo Dydensborg <mabd@dbc.dk>
2020-11-11 16:36:29 +0100

    Remove OPENAGENCY_URL variable

--- src/hive_corepo_testrunner/resource_manager.py
+++ src/hive_corepo_testrunner/resource_manager.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # -*- mode: python -*-
 """
@@ -78,7 +78,7 @@ class ContainerPoolImpl(ContainerSuitePool):
                         "HOLDINGSDB_URL": "",
                         "ADDISERVICE_URL": "",
                         "BATCHEXCHANGE_JDBCURL": "",
-                        "OPENAGENCY_URL": "http://%s:8080" % wiremock.get_ip(),
+                        "VIPCORE_ENDPOINT": "http://vipcore.iscrum-vip-extern-test.svc.cloud.dbc.dk/1.0/api/",
                         "HIVE_POOLSIZE": 1,
                         "HARVEST_POLLINTERVAL":2,
                         "LOG__dk_dbc": "TRACE"}
